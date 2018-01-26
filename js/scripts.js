0cb4c8778ae7776d0867969e88af1100c4e593c0
Sara Hamilton <sara@indeterminate.net>
2018-01-26 08:53:23 -0800

    add pizza constructor and pizza prototype for cost

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// business logic
+function Pizza {
+  this.toppings = [];
+  this.size = size;
+}
+
+Pizza.prototype.cost = function(toppings, size) {
+  return ((toppings.length * toppingsPrice) + sizePrice))
+}
+
+
+// business logic
