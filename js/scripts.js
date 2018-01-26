ad1b593dc4e8f4afa24516d8c52aa68a3dd82c09
Sara Hamilton <sara@indeterminate.net>
2018-01-26 09:19:18 -0800

    add initial jquery user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,34 @@ function Pizza {
 }
 
 Pizza.prototype.cost = function(toppings, size) {
+  var toppingsPrice = .75;
+  var sizePrice = 0;
+  if (size === personal) {
+    sizePrice = 7.99;
+  } else if (size === large) {
+    sizePrice = 12.99;
+  } else if (size === family) {
+    sizePrice = 15.99;
+  }
   return ((toppings.length * toppingsPrice) + sizePrice))
 }
 
 
 // business logic
+$(document).ready(function(){
+  $("#orderForm").submit(function(event){
+    event.preventDefault();
+
+    var pizzaTopping = "";
+    var pizzaToppings = [];
+    var pizzaSize = "";
+    var cost = 0;
+
+    $("input:checkbox[name=topping]:checked").each(function(){
+      pizzaTopping = $(this).val();
+      pizzaToppings.push(pizzaTopping);
+    });
+    pizzaSize = $("select#size").val();
+
+  });
+});
