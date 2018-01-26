5488dbee1eb73773e56d1dc457010521bf8680d7
Sara Hamilton <sara@indeterminate.net>
2018-01-26 14:57:10 -0800

    add prototypes and jquery functionality for customer and order

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function Pizza(toppings, size) {
 function Order() {
   this.pizzas = [];
   this.total = 0;
-  this.name = "";
+  this.customer = "";
 }
 
 function Customer(firstName, lastName, street, city, state, zipcode) {
@@ -33,11 +33,23 @@ Pizza.prototype.cost = function() {
 }
 
 Pizza.prototype.pizzaSummary = function() {
-  return this.size + " pizza with " + this.toppings
+  return (this.size + " Pizza with " + this.toppings);
+}
+
+Order.prototype.setCustomer = function(Customer) {
+  this.customer = Customer;
 }
 
 Order.prototype.adjustTotal = function(amount) {
   this.total = (parseFloat(this.total) + parseFloat(amount)).toFixed(2);
+}
+
+Customer.prototype.getName = function() {
+  return (this.firstName + " " + this.lastName);
+}
+
+Customer.prototype.getAddress = function() {
+  return (this.street + ", " + this.city + ", " + this.state + " " + this.zipcode);
 }
 
 
@@ -91,5 +103,14 @@ $(document).ready(function(){
     var zipcode = $("input#zipcode").val();
 
     var customer = new Customer(firstName, lastName, street, city, state, zipcode);
+    order.setCustomer(customer);
+    console.log(order);
+
+    $("#customerForm").hide();
+    $("#order-more").hide();
+    $("#checkout").hide();
+    $("#customer-name").text(customer.getName());
+    $("#customer-address").text(customer.getAddress());
+    $("#contact-info").show();
   });
 });
