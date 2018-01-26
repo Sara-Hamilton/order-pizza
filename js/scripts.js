dfc02c134060c7a21fb38be51f55aa3a956840d7
Sara Hamilton <sara@indeterminate.net>
2018-01-26 12:05:59 -0800

    add ability to order more than one pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 // business logic
-function Pizza (toppings, size){
+function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
+}
+
+function Order() {
+  this.pizzas = [];
+  this.total = 0;
+  this.name = "";
 }
 
 Pizza.prototype.cost = function() {
@@ -24,27 +30,42 @@ Pizza.prototype.orderSummary = function() {
 
 // business logic
 $(document).ready(function(){
+  var pizza = new Pizza();
+  var order = new Order();
   $("#orderForm").submit(function(event){
     event.preventDefault();
 
     var pizzaTopping = "";
     var pizzaToppings = [];
     var pizzaSize = "";
-    var cost = 0;
-    var pizzaOrder = new Pizza();
+    var cost = 0.00;
+    // var pizza = new Pizza();
+    // var order = new Order();
 
     $("input:checkbox[name=topping]:checked").each(function(){
       pizzaTopping = $(this).val();
       pizzaToppings.push(pizzaTopping);
     });
     pizzaSize = $("select#size").val();
-    pizzaOrder.toppings = pizzaToppings;
-    pizzaOrder.size = pizzaSize;
-    cost = parseFloat(pizzaOrder.cost()).toFixed(2);
-    $("#order").append(pizzaOrder.orderSummary());
-    $("#order").append("<h4>Total cost: $" + cost + "</h4>");
+    pizza.toppings = pizzaToppings;
+    pizza.size = pizzaSize;
+    cost = parseFloat(pizza.cost()).toFixed(2);
+    console.log(cost)
+    order.pizzas.push(pizza);
+    order.total += parseFloat(cost).toFixed(2);
+    console.log(pizza)
+    console.log(order)
+    console.log(typeof order.total)
+    $("#order").append(pizza.orderSummary());
+    $("#order").append("<h4>Price: $" + cost + "</h4>");
+    $("#total").append("<h4>$" + order.total + "</h4>")
 
-    $("#order-button").hide();
+    $("#orderForm").hide();
     $("#order-summary").show();
   });
+
+  $("#order-more").click(function() {
+    $("#orderForm").trigger("reset");
+    $("#orderForm").show();
+  })
 });
